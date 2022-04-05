38aa0c6910ab22783be390b7a2acf029060e803f
Zixuan Guo <guozixuan2000@gmail.com>
2022-04-05 01:50:14 -0500

    Unmap all lower-half pages

--- kernel/src/page.c
+++ kernel/src/page.c
@@ -13,8 +13,14 @@ typedef struct page_table_entry {
     bool present : 1;
     bool writable : 1;
     bool user : 1;
-    uint16_t unused : 9;
-    uint64_t address : 51;
+    bool write_through : 1;
+    bool cache_disable : 1;
+    bool accessed : 1;
+    bool dirty : 1;
+    bool page_size : 1;
+    uint8_t _unused0 : 4;
+    uintptr_t address : 40;
+    uint16_t _unused1 : 11;
     bool no_execute : 1;
 } __attribute__((packed)) pt_entry_t;
 
@@ -53,6 +59,8 @@ uint64_t read_cr0() {
 }
 
 void write_cr0(uint64_t value) { __asm__("mov %0, %%cr0" : : "r"(value)); }
+
+void write_cr3(uint64_t value) { __asm__("mov %0, %%cr3" : : "r"(value)); }
 
 void print_table_entry(pt_entry_t* page_entry) {
     // check if entry present
@@ -126,6 +134,48 @@ void pmem_free(uintptr_t p) {
     free_list.head = p;
 }
 
+// Unmap everything in the lower half of an address space with level 4 page table at address root
+void unmap_lower_half(uintptr_t root) {
+    // We can reclaim memory used to hold page tables, but NOT the mapped pages
+    pt_entry_t* l4_table = add_virtual_offset(root);
+    for (size_t l4_index = 0; l4_index < 256; l4_index++) {
+        // Does this entry point to a level 3 table?
+        if (l4_table[l4_index].present) {
+            // Yes. Mark the entry as not present in the level 4 table
+            l4_table[l4_index].present = false;
+
+            // Now loop over the level 3 table
+            pt_entry_t* l3_table = add_virtual_offset(l4_table[l4_index].address << 12);
+            for (size_t l3_index = 0; l3_index < 512; l3_index++) {
+                // Does this entry point to a level 2 table?
+                if (l3_table[l3_index].present && !l3_table[l3_index].page_size) {
+                    // Yes. Loop over the level 2 table
+                    pt_entry_t* l2_table = add_virtual_offset(l3_table[l3_index].address << 12);
+                    for (size_t l2_index = 0; l2_index < 512; l2_index++) {
+                        // Does this entry point to a level 1 table?
+                        if (l2_table[l2_index].present && !l2_table[l2_index].page_size) {
+                            // Yes. Free the physical page the holds the level 1 table
+                            pmem_free(l2_table[l2_index].address << 12);
+                        }
+                    }
+                    // Free the physical page that held the level 2 table
+                    pmem_free(l3_table[l3_index].address << 12);
+                }
+            }
+            // Free the physical page that held the level 3 table
+            pmem_free(l4_table[l4_index].address << 12);
+        }
+    }
+
+    // Reload CR3 to flush any cached address translations
+    write_cr3(read_cr3());
+}
+
+// reload Translation Lookaside Buffer
+void invalidate_tlb(uintptr_t virtual_address) {
+    __asm__("invlpg (%0)" ::"r"(virtual_address) : "memory");
+}
+
 void init_alloc(struct stivale2_struct_tag_memmap* memmap, struct stivale2_struct_tag_hhdm* hhdm) {
     free_list.head = NULL;  // init free list
     virtual_offset = hhdm->addr;
@@ -143,6 +193,8 @@ void init_alloc(struct stivale2_struct_tag_memmap* memmap, struct stivale2_struc
             }
         }
     }
+
+    unmap_lower_half(read_cr3());
 }
 
 bool vm_map(uintptr_t root, uintptr_t address, bool user, bool writable, bool executable) {
@@ -173,7 +225,7 @@ bool vm_map(uintptr_t root, uintptr_t address, bool user, bool writable, bool ex
             if (new_page == NULL) {  // run out of page
                 return false;
             }
-            memset(new_page, 0, PAGE_SIZE);
+            memset(add_virtual_offset(new_page), 0, PAGE_SIZE);
 
             kprintf(" new page %p shift %p\n", new_page, new_page >> 12);
 
@@ -187,6 +239,8 @@ bool vm_map(uintptr_t root, uintptr_t address, bool user, bool writable, bool ex
 
         table_entry = page_entry->address << 12;
     }
+
+    invalidate_tlb(address);
 
     return true;
 }
@@ -221,6 +275,8 @@ bool vm_protect(uintptr_t root, uintptr_t address, bool user, bool writable, boo
 
         table_entry = page_entry->address << 12;
     }
+
+    invalidate_tlb(address);
 
     return true;
 }
