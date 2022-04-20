038a26219ace432a6f45a32bf45e016e2fe2871c
Zixuan Guo <guozixuan2000@gmail.com>
2022-04-19 22:27:01 -0500

    support uppercase input

--- kernel/src/keyboard.c
+++ kernel/src/keyboard.c
@@ -2,19 +2,30 @@
 #include "kstdio.h"
 #include "pic.h"
 #include "port.h"
+#include "stdbool.h"
 
 #define BUFFER_SIZE 32
 
 // keyboard scan code look up table
 int scancode_table[] = {
     // first line
-    0, 27, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8, 9,
+    NULL, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 8, 9,
     // second line
-    113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 10, 0,
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 10, NULL,
     // third line
-    97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 96,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
     // fourth line
-    0, 92, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 0};
+    NULL, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', NULL};
+
+int upper_scancode_table[] = {
+    // first line
+    NULL, 27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 8, 9,
+    // second line
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 10, NULL,
+    // third line
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
+    // fourth line
+    NULL, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', NULL};
 
 // circular buffer
 volatile int buffer[BUFFER_SIZE];
@@ -22,17 +33,43 @@ volatile int buffer_start = 0;
 volatile int buffer_end = 0;
 volatile int buffer_count = 0;
 
+// modifiers status
+volatile bool lshift_pressed = false;
+volatile bool rshift_pressed = false;
+volatile bool capslock_pressed = false;
+
 __attribute__((interrupt)) void keyboard_handler(interrupt_context_t *ctx) {
     uint8_t scancode = inb(0x60);  // read a keyboard scan code
 
-    // only handle press event for now
-    if (scancode <= 0x58) {
-        // add to buffer when it is not full
-        if (buffer_count < BUFFER_SIZE) {
-            buffer[buffer_end] = scancode_table[scancode];
-            buffer_end = (buffer_end + 1) % BUFFER_SIZE;
-            buffer_count += 1;
-        }
+    switch (scancode) {
+        case 0x2A:  // left shift pressed
+            lshift_pressed = true;
+            break;
+        case 0x36:  // right shift pressed
+            rshift_pressed = true;
+            break;
+        case 0xAA:  // left shift released
+            lshift_pressed = false;
+            break;
+        case 0xB6:  // right shift released
+            rshift_pressed = false;
+            break;
+        case 0x3A:  // capslock pressed
+            capslock_pressed = !capslock_pressed;
+            break;
+        default:
+            // ignore key release event
+            if (scancode > 0x58) {
+                break;
+            }
+            // add to buffer when it is not full
+            if (buffer_count < BUFFER_SIZE) {
+                buffer[buffer_end] = (lshift_pressed || rshift_pressed || capslock_pressed)
+                                         ? upper_scancode_table[scancode]
+                                         : scancode_table[scancode];
+                buffer_end = (buffer_end + 1) % BUFFER_SIZE;
+                buffer_count += 1;
+            }
     }
 
     outb(PIC1_COMMAND, PIC_EOI);  // end of interrupt message
